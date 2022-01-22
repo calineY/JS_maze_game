e3eabbdf669c8811021d96127c52a1d3dc681c32
Caline <calineyammine123@gmail.com>
2022-01-22 23:28:42 +0200

    Added basic game functions

--- game.js
+++ game.js
@@ -1,12 +1,16 @@
 window.onload = function () {
     var s = document.getElementById("start");
+    var e = document.getElementById("end");
     var status = document.getElementById("status");
     var boundaries = document.getElementsByClassName("boundary");
     var first_hover = true;
+    var status_var = "start";
     function mouseOverElement() {
+        status_var = "ingame";
+        status.innerHTML = "Good luck";
         if (first_hover == false) {
             for (var i = 0; i < boundaries.length; i++) {
-                boundaries[i].style.background = "#eeeee";
+                boundaries[i].style.background = "#eeeeee";
             }
         } else {
             first_hover = false;
@@ -18,8 +22,16 @@ window.onload = function () {
                 for (var i = 0; i < boundaries.length; i++) {
                     boundaries[i].style.background = "red";
                 }
+                status_var = "lost";
             }
         });
     }
+    function youWin() {
+        if (status_var == "ingame") {
+            status.innerHTML = "You win";
+        }
+    }
     s.addEventListener("mouseover", mouseOverElement);
+    e.addEventListener("mouseover", youWin)
+
 }
