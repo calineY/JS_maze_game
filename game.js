601c9722a5205b57887b0cb9ce4f09b82ffebb51
Caline <calineyammine123@gmail.com>
2022-01-23 00:53:50 +0200

    Added score and made game reset after win

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ window.onload = function () {
     var boundaries = document.getElementsByClassName("boundary");
     var first_hover = true;
     var status_var = "start";
+    var score = 0;
     function mouseOverElement() {
         status_var = "ingame";
         status.innerHTML = "Good luck";
@@ -15,23 +16,32 @@ window.onload = function () {
         } else {
             first_hover = false;
         }
+
         document.addEventListener("mousemove", function (e) {
             var cursor = e.target.classList.value;
-            if (cursor == "boundary") {
-                status.innerHTML = "You lost";
+            if (cursor == "boundary" || cursor == "boundary example" && status_var == "ingame") {
                 for (var i = 0; i < boundaries.length; i++) {
                     boundaries[i].style.background = "red";
                 }
+                score -= 10;
+                status.innerHTML = "You lost" + "_Score:" + score;
                 status_var = "lost";
             }
         });
     }
     function youWin() {
         if (status_var == "ingame") {
-            status.innerHTML = "You win";
+            score += 5;
+            status.innerHTML = "You win" + "_Score:" + score;
+            status_var = "start";
         }
     }
+    function resetGame() {
+        score = 0;
+        status.innerHTML = "Game restarted";
+    }
     s.addEventListener("mouseover", mouseOverElement);
-    e.addEventListener("mouseover", youWin)
+    e.addEventListener("mouseover", youWin);
+    s.addEventListener("click", resetGame)
 
 }
