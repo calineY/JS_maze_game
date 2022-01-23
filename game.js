e09861b3472846ca7012dbd63e04d06649fdd395
Caline <calineyammine123@gmail.com>
2022-01-23 02:40:12 +0200

    Added comments

--- game.js
+++ game.js
@@ -6,8 +6,8 @@ window.onload = function () {
     var boundaries = document.getElementsByClassName("boundary");
     var extra_wall = document.getElementsByClassName("boundary example");
     var bottom_text = document.getElementsByTagName("p");
-    var first_hover = true;
-    var status_var = "start";
+    var first_hover = true;     //used to not loop over divs on start
+    var status_var = "start";    //used to keep track of game state
     var score = 0;
 
     function mouseOverElement() {
@@ -20,7 +20,7 @@ window.onload = function () {
         } else {
             first_hover = false;
         }
-        mouseOnBorder();
+        mouseOnBorder(); //checks if cursor touched the boundaries
     }
 
     function mouseOnBorder() {
@@ -60,7 +60,7 @@ window.onload = function () {
     s.addEventListener("mouseover", mouseOverElement);
     e.addEventListener("mouseover", youWin);
     s.addEventListener("click", resetGame);
-    blockRightSideOfEnd();
+    blockRightSideOfEnd();  //positioned the example wall to block right side of End div, so the user can't access it from the outer side of the wals
     bottom_text[0].innerHTML = "The object of this game is to guide the mouse cursor through the start area and get to the end area. Be sure to avoid the walls."; //changed punctuation
 
 
