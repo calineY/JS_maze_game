39a567dc3996b908227fad60b7b94540f5605fa2
Caline <calineyammine123@gmail.com>
2022-01-23 02:19:20 +0200

    Blocked right exposed side of end

--- game.js
+++ game.js
@@ -1,11 +1,15 @@
 window.onload = function () {
+
     var s = document.getElementById("start");
     var e = document.getElementById("end");
     var status = document.getElementById("status");
     var boundaries = document.getElementsByClassName("boundary");
+    var extra_wall = document.getElementsByClassName("boundary example");
+    var bottom_text = document.getElementsByTagName("p");
     var first_hover = true;
     var status_var = "start";
     var score = 0;
+
     function mouseOverElement() {
         status_var = "ingame";
         status.innerHTML = "Good luck";
@@ -16,10 +20,13 @@ window.onload = function () {
         } else {
             first_hover = false;
         }
+        mouseOnBorder();
+    }
 
+    function mouseOnBorder() {
         document.addEventListener("mousemove", function (e) {
             var cursor = e.target.classList.value;
-            if (cursor == "boundary" || cursor == "boundary example" && status_var == "ingame") {
+            if ((cursor == "boundary" || cursor == "boundary example") && status_var == "ingame") {
                 for (var i = 0; i < boundaries.length; i++) {
                     boundaries[i].style.background = "red";
                 }
@@ -29,6 +36,7 @@ window.onload = function () {
             }
         });
     }
+
     function youWin() {
         if (status_var == "ingame") {
             score += 5;
@@ -36,12 +44,24 @@ window.onload = function () {
             status_var = "start";
         }
     }
+
     function resetGame() {
         score = 0;
         status.innerHTML = "Game restarted";
     }
+
+    function blockRightSideOfEnd() {
+        extra_wall[0].style.position = "relative";
+        extra_wall[0].style.left = "263px";
+        extra_wall[0].style.bottom = "134px";
+        extra_wall[0].style.transform = "rotate(90deg)";
+    }
+
     s.addEventListener("mouseover", mouseOverElement);
     e.addEventListener("mouseover", youWin);
-    s.addEventListener("click", resetGame)
+    s.addEventListener("click", resetGame);
+    blockRightSideOfEnd();
+    bottom_text[0].innerHTML = "The object of this game is to guide the mouse cursor through the start area and get to the end area. Be sure to avoid the walls."; //changed punctuation
+
 
 }
